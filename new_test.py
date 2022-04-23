4cbcacc4ad51368d26393217a92d8802bd005d7f
AruviAravinds <74746860+AruviAravinds@users.noreply.github.com>
2022-04-23 14:40:54 +0530

    Add files via upload

--- new_test.py
+++ new_test.py
@@ -23,58 +23,62 @@ model = load_model('model_010960.h5')
 
 # plot source, generated and target images
 def plot_images(src_img, gen_img, tar_img):
-	images = vstack((src_img, gen_img, tar_img))
-	# scale from [-1,1] to [0,1]
-	images = (images + 1) / 2.0
-	titles = ['Source', 'Generated', 'Expected']
-	# plot images row by row
-	for i in range(len(images)):
-		# define subplot
-		pyplot.subplot(1, 3, 1 + i)
-		# turn off axis
-		pyplot.axis('off')
-		# plot raw pixel data
-		pyplot.imshow(images[i])
-		# show title
-		pyplot.title(titles[i])
-	pyplot.show()
+    images = vstack((src_img, gen_img, tar_img))
+    # scale from [-1,1] to [0,1]
+    images = (images + 1) / 2.0
+    titles = ['Source', 'Generated', 'Expected']
+    # plot images row by row
+    for i in range(len(images)):
+        # define subplot
+        pyplot.subplot(1, 3, 1 + i)
+        # turn off axis
+        pyplot.axis('off')
+        # plot raw pixel data
+        pyplot.imshow(images[i])
+        # show title
+        pyplot.title(titles[i])
+    pyplot.show()
 
 
 def load_images(path, size=(256,512)):
-	src_list, tar_list = list(), list()
-	# enumerate filenames in directory, assume all are images
-	for filename in listdir(path):
-		# load and resize the image
-		pixels = load_img(path + filename, target_size=size)
-		# convert to numpy array
-		pixels = img_to_array(pixels)
-		# split into satellite and map
-		sat_img, map_img = pixels[:, :256], pixels[:, 256:]
-		src_list.append(sat_img)
-		tar_list.append(map_img)
-	return [asarray(src_list), asarray(tar_list)]
+    
+    src_list, tar_list = list(), list()
+    # enumerate filenames in directory, assume all are images
+  
+    print ("The value of ix is" , (ix))
+    # load and resize the image
+    pixels = load_img(path, target_size=size)
+    # convert to numpy array
+    pixels = img_to_array(pixels)
+    # split into satellite and map
+    sat_img, map_img = pixels[:, :256], pixels[:, 256:]
+    src_list.append(sat_img)
+    tar_list.append(map_img)
+    return [asarray(src_list), asarray(tar_list)]
 
 def preprocess_data(data):
-	# load compressed arrays
-	# unpack arrays
-	X1, X2 = data[0], data[1]
-	# scale from [0,255] to [-1,1]
-	X1 = (X1 - 127.5) / 127.5
-	X2 = (X2 - 127.5) / 127.5
-	return [X1, X2]
+    # load compressed arrays
+    # unpack arrays
+    X1, X2 = data[0], data[1]
+    # scale from [0,255] to [-1,1]
+    X1 = (X1 - 127.5) / 127.5
+    X2 = (X2 - 127.5) / 127.5
+    return [X1, X2]
 
 # dataset path
-path = 'C:/maps/val/'
+ix = 5
+path = 'C:/maps/val/'+ str(int(ix)) +'.jpg'
 # load dataset
+
+
 [src_images, tar_images] = load_images(path)
 print('Loaded: ', src_images.shape, tar_images.shape)
 
 data = [src_images, tar_images]
 dataset = preprocess_data(data)
 [X1,X2]=dataset
-ix = randint(1, len(X1)+1,1)
-print ("The value of ix is" , (ix))
-src_image, tar_image = X1[ix], X2[ix]
+
+src_image, tar_image = X1,X2
 # generate image from source
 gen_image = model.predict(src_image)
 # plot all three images
